155e1afe3b550bca71d41e0d859dba57b2881b90
rsantiagopaz <rsantiagopaz@gmail.com>
2018-09-08 16:05:08 -0300

    x

--- sical fuentes.ori/sical/bin/_phpincludes/_tipos_titulos.php
+++
@@ -1,9 +0,0 @@
-<?php
- function traer_tipos_titulos(&$xml)
- { 		  	
-  	$sql="SELECT * ";
-	$sql.="FROM tipos_titulos ";				
-	$sql.="ORDER BY tipo";
-	toXML($xml, $sql, "tipostitulos");
- }
-?>
--- sical fuentes.ori/sical/bin/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-   $SERVIDOR = "localhost";
-   $USUARIO  = "root";
-   $PASSWORD = "";
-   $BASE     = "sical";
-   $link = @($GLOBALS["___mysqli_ston"] = mysqli_connect($SERVIDOR,  $USUARIO,  $PASSWORD));
-   @((bool)mysqli_query( $link, "USE $BASE"));
-   mysqli_query($GLOBALS["___mysqli_ston"], "SET NAMES 'utf8'");
-?>
